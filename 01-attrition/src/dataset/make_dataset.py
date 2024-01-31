fe6edb43365874be4e89cf824a401458e7481acc
artemgalyan <89996510+artemgalyan@users.noreply.github.com>
2024-01-31 14:46:19 +0300

    make dataset

---
+++ 01-attrition/src/dataset/make_dataset.py
@@ -0,0 +1,16 @@
+from pathlib import Path
+
+import click
+
+from pandas import get_dummies, read_csv
+
+
+@click.command()
+@click.argument('input_file', type=click.Path(exists=True))
+@click.argument('output_file', type=click.Path())
+def main(input_file: str, output_file: str) -> None:
+    assert Path(input_file).exists(), f'{input_file} do not exist'
+    df = read_csv(input_file)\
+        .drop(columns=['EmployeeID', 'Over18', 'EmployeeCount', 'StandardHours'])
+    df = get_dummies(df)
+    df.to_csv(output_file)
