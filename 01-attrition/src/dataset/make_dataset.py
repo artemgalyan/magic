fe71a3c6c9693a9a459add36989adafee242b21c
artemgalyan <89996510+artemgalyan@users.noreply.github.com>
2024-02-09 18:23:36 +0300

    removed not used columns

--- 01-attrition/src/dataset/make_dataset.py
+++ 01-attrition/src/dataset/make_dataset.py
@@ -10,7 +10,8 @@ from pandas import get_dummies, read_csv
 @click.argument('output_file', type=click.Path())
 def main(input_file: str, output_file: str) -> None:
     assert Path(input_file).exists(), f'{input_file} do not exist'
-    df = read_csv(input_file)\
-        .drop(columns=['Over18', 'EmployeeCount', 'StandardHours'])
+    df = read_csv(input_file) \
+        .drop(columns=['Over18', 'EmployeeCount', 'StandardHours']) \
+        .set_index('EmployeeID', drop=True, verify_integrity=True)
     df = get_dummies(df, drop_first=True)
     df.to_csv(output_file)
